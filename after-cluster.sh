e7308d409b4c8edd653f2efed16a5ffb543f0667
SimonGurney <simon.gurney@punksecurity.co.uk>
2023-03-07 14:56:46 +0000

    feat: generate user certs for ctf

--- after-cluster.sh
+++ after-cluster.sh
@@ -4,3 +4,11 @@ docker pull nginx:1.13 # used by config-serve
 # Add images here for them to be available at runtime
 # for example:
 # docker pull quay.io/kubernetes-ingress-controller/nginx-ingress-controller:0.9.0
+
+#setup certs
+mkdir /punk
+
+openssl genrsa -out /punk/punk.key 2048
+openssl req -new -key /punk/punk.key -out /punk/punk.csr -subj "/CN=punk/O=punk"
+openssl x509 -req -in /punk/punk.csr -CA /var/lib/minikube/certs/ca.crt -CAkey /var/lib/minikube/certs/ca.key -CAcreateserial -out  /punk/punk.crt -days 500
+#TODO remove ca key to prevent token minting
